f53566cfab666f0a683c6f4d8d68b2e4ac62177a
Inge Heeringa <inge.heeringa@gmail.com>
2023-03-18 11:35:51 +0100

    Test conditional rendering of nav link to SubmitPage

--- src/app/components/navigation/navigation.component.spec.ts
+++ src/app/components/navigation/navigation.component.spec.ts
@@ -1,13 +1,41 @@
 import { NavigationComponent } from "./navigation.component";
 import { render, screen } from "@testing-library/angular";
 import "@testing-library/jest-dom";
+import { provideMockStore } from "@ngrx/store/testing";
+import { selectIsLogged } from "../../store/user/user.reducer";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 
 describe("Given a Navigation component", () => {
   describe("When rendered", () => {
     test("Then it should show a link to 'Home'", async () => {
-      await render(NavigationComponent);
+      await render(NavigationComponent, {
+        imports: [HttpClientTestingModule, MatSnackBarModule],
+        providers: [
+          provideMockStore({
+            selectors: [{ selector: selectIsLogged, value: false }],
+          }),
+        ],
+      });
 
       const link = screen.getByRole("link", { name: /home/i });
+
+      expect(link).toBeInTheDocument();
+    });
+  });
+
+  describe("When rendered and the user is logged in", () => {
+    test("Then it should show a link to 'New post'", async () => {
+      await render(NavigationComponent, {
+        imports: [HttpClientTestingModule, MatSnackBarModule],
+        providers: [
+          provideMockStore({
+            selectors: [{ selector: selectIsLogged, value: true }],
+          }),
+        ],
+      });
+
+      const link = screen.getByRole("link", { name: /new post/i });
 
       expect(link).toBeInTheDocument();
     });
