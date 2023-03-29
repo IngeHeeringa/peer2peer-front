efab4848f7eb5e276093a24da8dd52a1ac53ad50
Inge Heeringa <inge.heeringa@gmail.com>
2023-03-29 14:44:06 +0200

    Add delete button to DetailsPage

--- src/app/pages/details-page/details-page.component.ts
+++ src/app/pages/details-page/details-page.component.ts
@@ -4,6 +4,7 @@ import { type Observable } from "rxjs";
 import { type Post } from "../../store/post/types";
 import { PostsService } from "../../services/posts/posts.service";
 import { UiService } from "../../services/ui/ui.service";
+import { UserService } from "../../services/user/user.service";
 
 @Component({
   selector: "app-details-page",
@@ -14,11 +15,13 @@ export class DetailsPageComponent {
   post$!: Observable<Post>;
   post!: Post;
   params!: Record<string, string>;
+  isLogged = this.userService.getIsLogged();
 
   constructor(
     @Inject(ActivatedRoute) private readonly route: ActivatedRoute,
     @Inject(PostsService) private readonly postsService: PostsService,
-    @Inject(UiService) private readonly uiService: UiService
+    @Inject(UiService) private readonly uiService: UiService,
+    @Inject(UserService) private readonly userService: UserService
   ) {}
 
   ngOnInit(): void {
@@ -34,5 +37,20 @@ export class DetailsPageComponent {
       this.post = data;
       this.uiService.hideLoading();
     });
+  }
+
+  deletePost() {
+    this.postsService.deletePostById(this.post.id);
+    this.uiService.redirectUser("");
+  }
+
+  allowAction() {
+    try {
+      const { username } = this.userService.checkUser();
+
+      return this.isLogged && this.post.creator === username;
+    } catch (error) {
+      return false;
+    }
   }
 }
--- src/app/pages/details-page/details.module.ts
+++ src/app/pages/details-page/details.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { DetailsRoutingModule } from "./details-routing.module";
 import { DetailsPageComponent } from "./details-page.component";
+import { MaterialModule } from "src/app/material/material.module";
 
 @NgModule({
   declarations: [DetailsPageComponent],
-  imports: [CommonModule, DetailsRoutingModule],
+  imports: [CommonModule, DetailsRoutingModule, MaterialModule],
 })
 export class DetailsModule {}
